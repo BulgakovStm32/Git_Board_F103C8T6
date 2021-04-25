ae7be70beeac0fbad45c6f5eae44f078136591d5
BulgakovStm32 <senyapukin@mail.ru>
2021-04-25 17:42:20 +0300

    Добавлене работа с АЦП

--- f103c8t6_MeasTrueRMS/My/Main/main.c
+++ f103c8t6_MeasTrueRMS/My/Main/main.c
@@ -74,122 +74,6 @@ uint8_t tic(uint16_t event){
 }
 //*******************************************************************************************
 //*******************************************************************************************
-//Работа с DS2782.
-
-#define DS2782_ADDR (0b00110100 << 1)//(0x34 << 1)
-#define DS2782_I2C	I2C1
-
-//************************************************************
-//Регистры DS2782.
-//typedef enum {
-//   Parameter_CONTROL         = 0x60, // Control Register
-//   Parameter_AB              = 0x61, // Accumulation Bias
-//   Parameter_AC              = 0x62, // Aging Capacity (2 bytes)
-//   Parameter_VCHG            = 0x64, // Charge Voltage
-//   Parameter_IMIN            = 0x65, // Minimum Charge Current
-//   Parameter_VAE             = 0x66, // Active Empty Voltage
-//   Parameter_IAE             = 0x67, // Active Empty Current
-//   Parameter_AE_40           = 0x68, // Active Empty 40
-//   Parameter_RSNSP           = 0x69, // Sense Resistor Prime
-//   Parameter_Full_40         = 0x6a, // Full 40 (2 bytes)
-//   Parameter_Full_Slope      = 0x6c, // Full Slope (4 bytes)
-//   Parameter_AE_Slope        = 0x70, // Active Empty Slope (4 bytes)
-//   Parameter_SE_Slope        = 0x74, // Standby Empty Slope (4 bytes)
-//   Parameter_RSGAIN          = 0x78, // Sense Resistor Gain (2 bytes)
-//   Parameter_RSTC            = 0x7a, // Sense Resistor Temperature Coefficient
-//   Parameter_FRSGAIN         = 0x7b, // Factory Gain (2 bytes)
-//   // Reserved, 0x7d
-//   Parameter_Address         = 0x7e, // 2-Wire Slave Address
-//   // Reserved, 0x7f
-// }DS2782_ParameterRegisters_t;
-//---------------------------
-typedef enum{
-	// Name                              Acc.   Size  Description
-	// Reserved, 0x00
-	Register_STATUS           = 0x01, // R/W    1     Status
-	Register_RAAC             = 0x02, // R      2     Remaining Active Absolute Capacity
-	Register_RSAC             = 0x04, // R      2     Remaining Standby Absolute Capacity
-	Register_RARC             = 0x06, // R      1     Remaining Active Relative Capacity
-	Register_RSRC             = 0x07, // R      1     Remaining Standby Relative Capacity
-	Register_IAVG             = 0x08, // R      2     Average Current
-	Register_TEMP             = 0x0a, // R      2     Temperature
-	Register_VOLT             = 0x0c, // R      2     Voltage
-	Register_CURRENT          = 0x0e, // R      2     Current
-	Register_ACR              = 0x10, // R/W    2     Accumulated Current (auto-saved)
-	Register_ACRL             = 0x12, // R      2     Low Accumulated Current
-	Register_AS               = 0x14, // R/W    1     Age Scalar (auto-saved)
-	Register_SFR              = 0x15, // R/W    1     Special Feature Register
-	Register_FULL             = 0x16, // R      2     Full Capacity
-	Register_AE               = 0x18, // R      2     Active Empty Capacity
-	Register_SE               = 0x1a, // R      2     Standby Empty Capacity
-	// Reserved, 0x1c - 0x1e
-	Register_EEPROM           = 0x1f, // R/W    1     EEPROM, Block 0
-	Register_User_EEPROM_1    = 0x20, // R/W    16    User EEPROM, Lockable, Block 0
-	Register_User_EEPROM_2    = 0x30, // R/W    8     Additional User EEPROM, Lockable, Block 0
-	// Reserved, 0x38 - 0x5f
-	//---------------------------------------------------
-	//Parameter EEPROM Memory Block 1
-	// R/W    32    Parameter EEPROM, Lockable, Block 1
-	Parameter_CONTROL         = 0x60, // Control Register
-	Parameter_AB              = 0x61, // Accumulation Bias
-	Parameter_AC              = 0x62, // Aging Capacity (2 bytes)
-	Parameter_VCHG            = 0x64, // Charge Voltage
-	Parameter_IMIN            = 0x65, // Minimum Charge Current
-	Parameter_VAE             = 0x66, // Active Empty Voltage
-	Parameter_IAE             = 0x67, // Active Empty Current
-	Parameter_AE_40           = 0x68, // Active Empty 40
-	Parameter_RSNSP           = 0x69, // Sense Resistor Prime
-	Parameter_Full_40         = 0x6a, // Full 40 (2 bytes)
-	Parameter_Full_Slope      = 0x6c, // Full Slope (4 bytes)
-	Parameter_AE_Slope        = 0x70, // Active Empty Slope (4 bytes)
-	Parameter_SE_Slope        = 0x74, // Standby Empty Slope (4 bytes)
-	Parameter_RSGAIN          = 0x78, // Sense Resistor Gain (2 bytes)
-	Parameter_RSTC            = 0x7a, // Sense Resistor Temperature Coefficient
-	Parameter_FRSGAIN         = 0x7b, // Factory Gain (2 bytes)
-	// Reserved, 0x7d
-	Parameter_Address         = 0x7e, // 2-Wire Slave Address
-	// Reserved, 0x7f
-	//---------------------------------------------------
-	// Reserved, 0x80 - 0xef
-	Register_Unique_ID        = 0xf0, // R      8     Unique ID (factory option)
-	// Reserved, 0xf8 - 0xfd
-	Register_Function_Command = 0xfe, // W      1     Function Command
-	// Reserved, 0xff
- }DS2782_Registers_t;
-
-//---------------------------
-typedef struct{
-	uint16_t hour;
-	uint8_t  min;
-	uint8_t  sec;
-}DS2782_t;
-
-static DS2782_t DS2782Str;
-//---------------------------
-
-
-//************************************************************
-uint16_t DS2782_ReadADC(DS2782_Registers_t addrReg, uint8_t len){
-
-	uint8_t rxBuf[2] = {0};
-	//-------------------
-	//Этот кусок кода работает. Обмен с DS2782 есть.
-//	I2C_StartAndSendDeviceAddr(DS2782_I2C, DS2782_ADDR | I2C_MODE_WRITE);
-//	I2C_SendData(DS2782_I2C, &regAddr, 1);
-//	I2C_Stop(DS2782_I2C);
-
-	//-------------------
-	I2C_Read(DS2782_I2C, DS2782_ADDR, (uint8_t)addrReg, rxBuf, len);
-
-	return ((rxBuf[1] << 8) | rxBuf[0]);
-}
-//************************************************************
-void Coulomb_Calc(uint16_t current){
-
-
-}
-//*******************************************************************************************
-//*******************************************************************************************
 int main(void){
 
 	uint16_t dsRes = 0;
@@ -198,6 +82,7 @@ int main(void){
 	//Drivers.
 	Sys_Init();
 	Gpio_Init();
+	Adc_Init();
 	//***********************************************
 	SysTick_Init();
 	__enable_irq();
@@ -257,85 +142,22 @@ int main(void){
 			Lcd_String(21, 1);
 			if(Blink(INTERVAL_50_mS))Lcd_Chr(':');
 			else                     Lcd_Chr(' ');
+			//***********************************************
+			//Работа с АЦП.
+			//Регулярный канал АЦП.
+			Lcd_String(1, 5);
+			Lcd_Print("RegularADC=");
+			//Lcd_BinToDec(Adc_GetMeas(0), 4, LCD_CHAR_SIZE_NORM);
+			Lcd_BinToDec(Adc_GetRegDR(ADC1), 4, LCD_CHAR_SIZE_NORM);
 
+
+			//Инжектированный канал АЦП.
+			Lcd_String(1, 6);
+			Lcd_Print("InjectedADC=");
+			Lcd_BinToDec(Adc_GetRegJDRx(ADC1, 4), 4, LCD_CHAR_SIZE_NORM);
+			//***********************************************
 			Lcd_Update();//вывод сделан для SSD1306
 			Lcd_Clear();
-			//***********************************************
-			//Работа с микросхемой DS2782.
-
-			//Чтение адреса DS2782.
-//			Lcd_String(1, 5);
-//			Lcd_Print("DS2782_ADDR:");
-//			Lcd_Print("0x");
-//			Lcd_u8ToHex(DS2782_ReadADC(Register_Unique_ID, 1) >> 1);
-
-			//получение температуры.
-			uint16_t Temp   = DS2782_ReadADC(Register_TEMP, 2);
-			         Temp   = ( ((Temp << 8) & 0xFF00) | ((Temp >> 8) & 0x00FF) );
-			         Temp >>= 5;
-
-			uint32_t tTemp =  Temp * 125;
-					 tTemp = (tTemp + 50) / 100;
-
-			Lcd_String(1, 5);
-			Lcd_Print("Bat_T  =");
-			Lcd_BinToDec(tTemp/10, 2, LCD_CHAR_SIZE_NORM);
-			Lcd_Chr('.');
-			Lcd_BinToDec(tTemp%10, 1, LCD_CHAR_SIZE_NORM);
-			Lcd_Print(" C");
-
-			//получение напряжения на АКБ.
-			uint16_t voltTemp = DS2782_ReadADC(Register_VOLT, 2);
-			voltTemp = ( ((voltTemp << 8) & 0xFF00) | ((voltTemp >> 8) & 0x00FF) );
-
-			uint32_t adcTemp = voltTemp >> 5;   //
-
-			adcTemp  &= 0b0000001111111111;//Уберем знак
-			adcTemp  *= 488;               //это 4,88mV * 100. Это нужно чтобы избавится от запятой => получили микровольты
-			//adcTemp = ((adcTemp + 50) / 100);
-			adcTemp  *= 5500; //5475;              //это коэф-т деления резистивного делителя, умноженного на 1000.
-			adcTemp = ((adcTemp + 500000) / 1000000);
-
-			Lcd_String(1, 6);
-			Lcd_Print("Bat_U  =");
-			Lcd_BinToDec(adcTemp / 100, 2, LCD_CHAR_SIZE_NORM);
-			Lcd_Chr(',');
-			Lcd_BinToDec(adcTemp % 100, 2, LCD_CHAR_SIZE_NORM);
-			Lcd_Chr('V');
-
-			//Получения тока потребления от АКБ.
-			int16_t currentTemp = DS2782_ReadADC(Register_CURRENT, 2);
-			//int16_t currentTemp = DS2782_ReadADC(Register_IAVG, 2);
-			uint8_t currentSign = 0;
-			currentTemp = ( ((currentTemp << 8) & 0xFF00) | ((currentTemp >> 8) & 0x00FF) );
-
-			if(currentTemp < 0)
-				{
-					currentTemp = (currentTemp ^ 0xffff) + 1;	//Уберем знак
-					currentSign = 1;
-				}
-
-			uint32_t currentAdcTemp = currentTemp;
-			currentAdcTemp *= 1563; //
-			currentAdcTemp  = ((currentAdcTemp + 5000) / 10000);
-
-			Lcd_String(1, 7);
-			Lcd_Print("Bat_I  =");
-			if(currentSign)Lcd_Chr('-');
-			else           Lcd_Chr(' ');
-
-			Lcd_BinToDec(currentAdcTemp, 4, LCD_CHAR_SIZE_NORM);
-			Lcd_Print("mA");
-
-			//Расчет кулонов.
-			uint16_t coulombTemp = 0;
-
-			Coulomb_Calc(currentAdcTemp);
-
-			Lcd_String(1, 8);
-			Lcd_Print("Coulomb=");
-			Lcd_BinToDec(coulombTemp, 4, LCD_CHAR_SIZE_NORM);
-			Lcd_Print("q");
 			//***********************************************
 			/* Sleep */
 			//__WFI();
@@ -361,4 +183,4 @@ void SysTick_Handler(void){
 	Encoder()->Loop();
 }
 //*******************************************************************************************
-//*******************************************************************************************
+//******************************************************************************************
