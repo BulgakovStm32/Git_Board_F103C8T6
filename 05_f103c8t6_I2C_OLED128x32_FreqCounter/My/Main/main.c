105c680a99aa7917ac6427faf57d357ad9f1dcc3
BulgakovStm32 <senyapukin@mail.ru>
2021-11-18 22:59:50 +0300

    Добавлен модуль MyString

--- 05_f103c8t6_I2C_OLED128x32_FreqCounter/My/Main/main.c
+++ 05_f103c8t6_I2C_OLED128x32_FreqCounter/My/Main/main.c
@@ -176,73 +176,6 @@ void Task_Spi(void){
 	Scheduler_SetTimerTask(Task_Spi, 100);
 }
 //************************************************************
-#define TXT_BUF_SIZE	128
-
-typedef struct{
-	uint8_t  buf[TXT_BUF_SIZE];
-	uint16_t bufIndex;
-}TxtBuf_t;
-
-TxtBuf_t	TxtBuf;
-//-----------------------------
-void Txt_Chr(uint8_t ch){
-
-  if(TxtBuf.bufIndex < TXT_BUF_SIZE) TxtBuf.buf[TxtBuf.bufIndex++] = ch;
-}
-//-----------------------------
-uint8_t Txt_Print(char *txt){
-
-  uint8_t i = 0;
-  //--------------------
-  while(*txt)
-    {
-	  Txt_Chr(*txt++);
-      i++;
-    }
-  //Txt_Chr('\n');
-  i++;
-  return i;
-}
-//-----------------------------
-uint8_t Txt_BinToDec(uint32_t var, uint32_t num){
-
-	uint8_t decArray[10];
-	//--------------------
-	decArray[9] = (uint8_t)(var/1000000000);
-	var %= 1000000000;
-
-	decArray[8] = (uint8_t)(var/100000000);
-	var %= 100000000;
-
-	decArray[7] = (uint8_t)(var/10000000);
-	var %= 10000000;
-
-	decArray[6] = (uint8_t)(var/1000000);
-	var %= 1000000;
-
-	decArray[5] = (uint8_t)(var/100000);
-	var %= 100000;
-
-	decArray[4] = (uint8_t)(var/10000);
-	var %= 10000;
-
-	decArray[3] = (uint8_t)(var/1000);
-	var %= 1000;
-
-	decArray[2] = (uint8_t)(var/100);
-	var %= 100;
-
-	decArray[1] = (uint8_t)(var/10);
-	decArray[0] = (uint8_t)(var%10);
-	//--------------------
-	//Вывод на дисплей
-	for(uint8_t i=0; i<num; i++)
-	{
-		Txt_Chr(decArray[(num-1)-i]+'0');
-	}
-	return num+1;
-}
-//-----------------------------
 void Task_Uart(void){
 
 //	Txt_Chr('\f');//Очистка окна терминала
@@ -269,12 +202,16 @@ void Task_Uart(void){
 	Txt_BinToDec(1, 1);
 	Txt_Print(" C");
 	Txt_Chr('\n');
+
+	Txt_Print("TIM3_CNT= ");
+	Txt_BinToDec(EncoderReg, 4);
+	Txt_Chr('\n');
 	Txt_Chr('\n');
 
-	DMA1Ch4StartTx(TxtBuf.buf, TxtBuf.bufIndex);
-	TxtBuf.bufIndex = 0;
+	DMA1Ch4StartTx(Txt_Buf()->buf, Txt_Buf()->bufIndex);
+	Txt_Buf()->bufIndex = 0;
 	//-----------------------------
-	Scheduler_SetTimerTask(Task_Uart, 1000);
+	Scheduler_SetTimerTask(Task_Uart, 50);
 }
 //*******************************************************************************************
 //*******************************************************************************************
