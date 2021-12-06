2299abddef1a8bd8cf80caf6ac24c4181bb32397
Anton Belyaev <senyapukin@mail.ru>
2021-12-06 16:04:28 +0300

    Изменена работа с ADS1115.

--- 06_f103c8t6_I2C_OLED128x32_adc_ADS1115/My/Main/main.c
+++ 06_f103c8t6_I2C_OLED128x32_adc_ADS1115/My/Main/main.c
@@ -4,10 +4,7 @@
  *  Created on: 19 октября 2021 года.
  *  Autho     : Беляев А.А.
  *
- *	Описание        : Трехканальный измеритель температуры.
- *  Датчики         : DS18B20 - 3шт.
- *  Вывод информации: OLED дисплей SSD1306 128x64;
- *      			  UART1(PA9-U1TX, PA10-U1RX), скорость 9600.
+ *	Описание  : Работа с АЦП ADS1115.
  */
 //*******************************************************************************************
 //*******************************************************************************************
@@ -22,12 +19,14 @@ typedef struct{
 }Time_t;
 
 static Time_t Time;
-//*******************************************************************************************
-//*******************************************************************************************
-void IncrementOnEachPass(uint32_t *var, uint16_t event){
 
-		   uint16_t riseReg  = 0;
-	static uint16_t oldState = 0;
+ADS1115_t	ADS1115;
+//*******************************************************************************************
+//*******************************************************************************************
+void IncrementOnEachPass(uint32_t *var, uint32_t event){
+
+		   uint32_t riseReg  = 0;
+	static uint32_t oldState = 0;
 	//-------------------
 	riseReg  = (oldState ^ event) & event;
 	oldState = event;
@@ -54,12 +53,13 @@ uint32_t Led_Blink(uint32_t millis, uint32_t period, uint32_t switch_on_time){
 	return flag;
 }
 //************************************************************
-void Time_Display(uint8_t cursor_x, uint8_t cursor_y){
+void Time_Display(uint32_t cursor_x, uint32_t cursor_y){
 
+	static const char Text_Time[] = "Time:";
 	//Вывод времени.
 	Lcd_SetCursor(cursor_x, cursor_y);
-	Lcd_Print("Time: ");
-	Lcd_BinToDec(Time.hour, 2, LCD_CHAR_SIZE_NORM);//часы
+	Lcd_Print((char*)Text_Time);
+	Lcd_BinToDec(Time.hour, 2, LCD_CHAR_SIZE_NORM); //часы
 	Lcd_Chr(':');
 	Lcd_BinToDec(Time.min,  2, LCD_CHAR_SIZE_NORM); //минуты
 	Lcd_Chr(':');
@@ -68,17 +68,36 @@ void Time_Display(uint8_t cursor_x, uint8_t cursor_y){
 //*******************************************************************************************
 //*******************************************************************************************
 //Работа с АЦП ADS1115.
-
-
-
+const char Text_Header[]  = "_ADS1115_";
+const char Text_I2cAddr[] = "ADS115_I2C_ADDR:";
+const char Text_AdcCode[] = "ADS115_ADC_CODE=";
 
 //************************************************************
 //Работа с микросхемой ADS1115.
 void Task_ADS1115(void){
 
-	ADS1115_SelectInput(0);
-	uint16_t adcTemp = ADS1115_GetData();
+	ADS1115_SelectChannel(&ADS1115, ADS1115_MUX_AIN0_GND);
+	ADS1115_ReadAdcData(&ADS1115);
+}
+//************************************************************
+void Task_Display_ADS1115(void){
 
+	//Шапка
+	Lcd_SetCursor(1, 1);
+	Lcd_Print((char*)Text_Header);
+
+	//Вывод времени.
+	Time_Display(1, 2);
+
+	//Вывод адреса на шине I2C
+	Lcd_SetCursor(1, 3);
+	Lcd_Print((char*)Text_I2cAddr);
+	Lcd_u8ToHex(ADS1115.I2cAddr);
+
+	//Вывод значения АЦП.
+	Lcd_SetCursor(1, 4);
+	Lcd_Print((char*)Text_AdcCode);
+	Lcd_BinToDec(ADS1115.ConversionReg, 6, LCD_CHAR_SIZE_NORM);
 }
 //*******************************************************************************************
 //*******************************************************************************************
@@ -183,37 +202,18 @@ void Task_Lcd_DS2782(void){
 	Lcd_Print(" Vdd=");
 	Lcd_BinToDec(AdcMeas.Vdd_V, 5, LCD_CHAR_SIZE_NORM);
 }
-//************************************************************
-void Task_Lcd(void){
-
-	//Мигание светодиодами.
-//	if(Led_Blink(RTOS_GetTickCount(), 1000, 50)) LedPC13On();
-//	else										 LedPC13Off();
-
-	//Шапка
-	Lcd_SetCursor(1, 1);
-	Lcd_Print("_THERMOMETER_(+BT)");
-
-	//Вывод времени.
-	Time_Display(1, 2);
-
-	//-----------------------------
-	//Scheduler_SetTask(Task_Lcd);
-}
-//************************************************************
+//*******************************************************************************************
+//*******************************************************************************************
 void Task_LcdUpdate(void){
 
 	if(Led_Blink(RTOS_GetTickCount(), 1000, 50)) LedPC13On();
 	else										 LedPC13Off();
 
-	//RTOS_SetTask(Task_Lcd, 0, 0);
-	RTOS_SetTask(Task_Lcd_DS2782, 0, 0);
-
-	Lcd_Update(); //вывод сделан для SSD1306
-	Lcd_ClearVideoBuffer();
+	//RTOS_SetTask(Task_Lcd_DS2782, 0, 0);
+	RTOS_SetTask(Task_Display_ADS1115, 0, 0);
 	//-----------------------------
-	//Scheduler_SetTask(Task_LcdUpdate);
-	//Scheduler_SetTimerTask(Task_LcdUpdate, 1000);
+	Lcd_Update();          //вывод сделан для SSD1306
+	Lcd_ClearVideoBuffer();//
 	//LedPC13Toggel();
 }
 //*******************************************************************************************
@@ -229,14 +229,20 @@ int main(void){
 	//Uart1Init(USART1_BRR);
 	//Adc_Init();
 
-	microDelay(100000);//Эта задержка нужна для стабилизации напряжения патания.
+	microDelay(100000);//Эта задержка нужна для стабилизации напряжения питания.
 					   //Без задержки LCD-дисплей не работает.
 	//***********************************************
 	//Ини-я дисплея.
 	SSD1306_Init(SSD1306_I2C);
 	//***********************************************
 	//Ини-я АЦП ADS1115.
-	ADS1115_Init(ADS1115_I2C);
+	ADS1115.i2c        = ADS1115_I2C;
+	ADS1115.I2cAddr    = ADS1115_I2C_ADDR;
+	ADS1115.ConfigReg |= (ADS1115_MUX_AIN0_GND    << ADS1115_MUX_Pos) | //Выбор канала.
+						 (ADS1115_PGA_1           << ADS1115_PGA_Pos) | //Коэффициент усиления усилителя.
+						 (ADS1115_MODE_CONTINUOUS << ADS1115_MODE_Pos)| //Режим работы.
+						 (ADS1115_DR_8Hz          << ADS1115_DR_Pos);   //Частота дискретизации.
+	ADS1115_Init(&ADS1115);
 	//***********************************************
 	//Ини-я диспетчера и постановка задач в очередь.
 	RTOS_Init();
