1424cdd88a4f4da51a9659b1b79c188a17ddce1b
Anton Belyaev <senyapukin@mail.ru>
2023-02-09 18:53:44 +0300

    First commit

---
+++ 14_f103c8t6_BootLoaderPtocolEmulator/My/Main/main.h
@@ -0,0 +1,78 @@
+/*
+ * main.h
+ *
+ *  Created on: Dec 18, 2020
+ *      Author: belyaev
+ */
+//*******************************************************************************************
+//*******************************************************************************************
+#ifndef _MAIN_H_
+#define _MAIN_H_
+//-------------------------
+//Hardware.
+#include "stm32f103xb.h"
+#include "math.h"
+//-------------------------
+//Drivers.
+#include "sys_ST.h"
+#include "gpio_ST.h"
+#include "sys_tick_ST.h"
+#include "spi_ST.h"
+#include "i2c_ST.h"
+#include "timers_ST.h"
+#include "dma_ST.h"
+#include "adc_ST.h"
+#include "uart_ST.h"
+#include "flash_ST.h"
+//-------------------------
+//Планировщик
+//#include "Scheduler.h"
+//#include "rtos.h"
+//-------------------------
+//Application
+#include "Delay.h"
+//#include "LcdGraphic.h"
+//#include "Blink.h"
+//#include "DS18B20.h"
+//#include "Filters.h"
+//#include "AHT10.h"
+//#include "DS2782.h"
+//#include "EncoderMy.h"
+//
+//#include "ssd1306.h"
+//#include "MyString.h"
+//#include "PID.h"
+//#include "Time.h"
+//#include "Crc.h"
+//
+//#include "StringParser.h"
+//#include "HC-12.h"
+//#include "si5351.h"
+//#include "Config.h"
+//*******************************************************************************************
+//*******************************************************************************************
+#define FLAG_SET	1
+#define FLAG_CLEAR	0
+
+#define TRUE 		1
+#define FALSE 		0
+//*******************************************************************************************
+//*******************************************************************************************
+//extern void Reset_Handler(void);
+//int  main(void);
+void SysTick_IT_Handler(void);
+
+//*******************************************************************************************
+//*******************************************************************************************
+#endif /* CORE_INC_MAIN_H_ */
+
+
+
+
+
+
+
+
+
+
+
---
+++ 14_f103c8t6_BootLoaderPtocolEmulator/My/STM32_Perif/flash_ST.h
@@ -0,0 +1,50 @@
+/*
+ * flash_ST.h
+ *
+ *  Created on: 9 авг. 2022 г.
+ *      Author: belyaev
+ */
+
+#ifndef FLASH_ST_H
+#define FLASH_ST_H
+//*******************************************************************************************
+//*******************************************************************************************
+
+//#include "main.h"
+#include "stm32f103xb.h"
+
+//*******************************************************************************************
+//*******************************************************************************************
+//Как же определить свободную страницу? Это зависит от размера прошивки.
+//Обычно используют последнюю страницу в памяти.
+//Её адрес можно вычислить как [начальный адрес] + [номер страницы - 1] * [размер страницы].
+//Выберем 16 страницу: 0x0800 0000 + 15 * 1024 = 0x0800 3C00
+
+//#define FLASH_PAGES_NUM 			64		//Кол-во страниц флеш-памяти для микроконтроллера с  64kB FLASH.
+#define FLASH_PAGES_NUM				128 	//Кол-во страниц флеш-памяти для микроконтроллера с 128kB FLASH.
+#define FLASH_PAGE_SIZE				1024	//Размер страницы флеш-памяти в байтах для STM32F103xx.
+#define FLASH_PAGE_ADDR(pageNum)	(uint32_t)(FLASH_BASE + FLASH_PAGE_SIZE * pageNum)
+
+//Адреса страниц флеш-памяти
+//#define FLASH_PAGE_127 (FLASH_BASE + (FLASH_PAGE_SIZE * (FLASH_PAGE_NUM - 0)))
+//#define FLASH_PAGE_126 (FLASH_BASE + (FLASH_PAGE_SIZE * (FLASH_PAGE_NUM	- 1)))
+//
+//#define FLASH_PAGE127_END_ADDR 	(FLASH_PAGE_127 + FLASH_PAGE_SIZE)
+//#define FLASH_PAGE126_END_ADDR 	(FLASH_PAGE_126 + FLASH_PAGE_SIZE)
+//*******************************************************************************************
+//*******************************************************************************************
+void     STM32_Flash_Lock     (void);
+void     STM32_Flash_Unlock   (void);
+void     STM32_Flash_ErasePage(uint32_t pageAddress);
+void     STM32_Flash_WriteWord(uint32_t word, uint32_t address);
+uint32_t STM32_Flash_ReadWord (uint32_t address);
+
+void STM32_Flash_ReadBuf (void* Src, void* Dst, uint32_t size);
+void STM32_Flash_WriteBuf(void* Src, void* Dst, uint32_t size);
+//*******************************************************************************************
+//*******************************************************************************************
+#endif /* FLASH_ST_H */
+
+
+
+
